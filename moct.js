5462a0aec5a7e708687375e3f33468e0741ffcf5
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-11-16 08:16:44 -0500

    Finish 'order' command.

--- moct.js
+++ moct.js
@@ -1,7 +1,15 @@
-exports.info = require('./package.json');
-
-exports.order = function(inList) {
-    for (var i = 0; i <= inList.length(); i++) {
-        
-    }
+exports.order = function(inList, lts) {
+    let outList = [], inLength = inList.length(), expt;
+    while (outList.length() <= inLength) {
+        for (var i = 0; i <= inList.length; i++) {
+            if (!inList[i].isInteger()) { throw new Error ("Input list must contain numbers only."); }
+            if (expt < inList[i] && lts == true) {
+                expt = inList[i];
+            } else if (expt > inList[1] && lts == false) {
+                expt = inList[1]
+            }
+        };
+        outList.push(expt)
+    };
+    return outList;
 };
